39e43dc2ed0d400841c4573f8f011c84b2c5a067
mlaasri <mlaasridev@gmail.com>
2025-05-15 22:56:58 -0400

    Switched to color widgets in test_box

--- tests/widgets/test_box.py
+++ tests/widgets/test_box.py
@@ -12,26 +12,32 @@
 
 from .render_page import render_page
 import webwidgets as ww
-from webwidgets.compilation.html import Div, RawText
+from webwidgets.compilation.html import Div
 
 
 class TestBox:
 
-    # A simple text widget
-    class Text(ww.Widget):
+    # A simple color widget
+    class Color(ww.Widget):
+        def __init__(self, color: str):
+            super().__init__()
+            self.color = color
+
         def build(self):
-            return Div([RawText("Hello, World!")])
+            return Div(style={"background-color": self.color,
+                              "height": "100%",
+                              "width": "100%"})
 
     # A simple Page with one Box containing a variable number of Text widgets
     class SimplePage (ww.Page):
         def __init__(self, n: int = 2):
             box = ww.Box(ww.Direction.HORIZONTAL)
             for _ in range(n):
-                box.add(TestBox.Text())
+                box.add(TestBox.Color("red"))
             super().__init__(widgets=[box])
 
-    def test_box(self, web_drivers):
+    def test_box(self, web_drivers_info):
         page = TestBox.SimplePage()
-        for web_driver in web_drivers:
-            result = render_page(page, web_driver)
-            assert result.shape
+        for web_driver_info in web_drivers_info:
+            result = render_page(page, web_driver_info, size=(800, 800))
+            assert (result.shape[0], result.shape[1]) == (800, 800)
