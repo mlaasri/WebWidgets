59b8f27de07a621e294fa1dcc1869e163c41e2e3
mlaasri <mlaasridev@gmail.com>
2025-05-29 19:21:05 -0400

    Made color test generic

--- tests/widgets/test_render_page.py
+++ tests/widgets/test_render_page.py
@@ -11,7 +11,9 @@
 # =======================================================================
 
 import numpy as np
+import pytest
 from .render_page import render_page
+from typing import Tuple
 import webwidgets as ww
 
 
@@ -19,10 +21,15 @@ class TestRenderPage:
     """Test cases for the test utility function `render_page`.
     """
 
-    class Red(ww.Widget):
+    class Color(ww.Widget):
+        def __init__(self, color: Tuple[int, int, int]):
+            super().__init__()
+            self.color = color
+
         def build(self):
+            hex_color = "#%02x%02x%02x" % self.color
             return ww.compilation.html.Div(
-                style={"background-color": "red",
+                style={"background-color": hex_color,
                        "height": "100vh",
                        "width": "100vw"}
             )
@@ -36,11 +43,21 @@ class TestRenderPage:
             assert array.shape[1] >= 10
             assert array.shape[2] in (3, 4)  # Some drivers add alpha channel
 
-    def test_red_page(self, web_drivers):
-        page = ww.Page([TestRenderPage.Red()])
+    @pytest.mark.parametrize("color", [
+        (255, 0, 0),  # Red
+        (0, 255, 0),  # Green
+        (0, 0, 255),  # Blue
+        (0, 0, 0),  # Black
+        (128, 128, 128),  # Gray
+        (255, 255, 255),  # White
+        (123, 45, 67),  # Other color 1
+        (234, 0, 156)  # Other color 2
+    ])
+    def test_colored_page(self, color, web_drivers):
+        page = ww.Page([TestRenderPage.Color(color)])
         for web_driver in web_drivers:
             array = render_page(page, web_driver)
             assert isinstance(array, np.ndarray)
-            assert np.all(array[..., 0] == 255)
-            assert np.all(array[..., 1] == 0)
-            assert np.all(array[..., 2] == 0)
+            assert np.all(array[..., 0] == color[0])
+            assert np.all(array[..., 1] == color[1])
+            assert np.all(array[..., 2] == color[2])
